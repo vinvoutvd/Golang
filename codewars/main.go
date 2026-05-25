376f9567f334a6a9ed5a40428dae5df748f9dcd7
Vladimir Tretyakov <vinvoutvd@gmail.com>
2026-05-25 17:58:29 +0500

    Solution for "Josephus_Survivor" (codewars - 5kuy)

---
+++ codewars/main.go
@@ -0,0 +1,30 @@
+//https://www.codewars.com/kata/555624b601231dc7a400017a/train/go
+
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Josephus Survivor:")
+	fmt.Println(JosephusSurvivor(7,3))
+}
+
+func JosephusSurvivor(n, k int) int {
+	circle := make([]int, n)
+	for i := range(circle){
+		circle[i] = i+1
+	}
+	i := -1
+	j := 0
+	for len(circle)!=1{
+		i++
+		j++
+		i%=len(circle)
+		if j == k{
+			j=0
+			circle = append(circle[:i], circle[i+1:]...)
+			i--
+		}
+	}
+	return circle[0]
+}
