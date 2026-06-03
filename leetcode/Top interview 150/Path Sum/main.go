9733548cb94f1d9f61c85da6f20de2b90756246f
Vladimir Tretyakov <vinvoutvd@gmail.com>
2026-06-03 21:01:22 +0500

    Solution

---
+++ leetcode/Top interview 150/Path Sum/main.go
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/path-sum/?envType=study-plan-v2&envId=top-interview-150
+//Definition for a binary tree node.
+type TreeNode struct {
+    Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+// Решение с помощью рекурсии и введением дополнительной переменной total для сохранения промежуточных значений 
+func dfs (node *TreeNode, total int, target int) (bool){
+		if node == nil {
+			return false
+		}
+		total+=node.Val
+		if node.Left==nil && node.Right==nil{
+			return total == target
+		}
+	return dfs(node.Left, total, target) || dfs(node.Right, total, target)
+	}
+ 
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	return dfs(root, 0, targetSum)} 
